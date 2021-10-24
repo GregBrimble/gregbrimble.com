ef6f4ad95dbf2bb3c35e65e6fc6b06c706f50134
Greg Brimble <developer@gregbrimble.com>
2021-10-24 10:48:15 +0100

    Rm header

--- app/root.tsx
+++ app/root.tsx
@@ -56,9 +56,9 @@ export default function App() {
 
   return (
     <Document>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </Document>
   );
 }
