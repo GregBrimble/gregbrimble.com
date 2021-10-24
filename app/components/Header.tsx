d7aa207a04c7c1da99ee9251a47861110ab56934
Greg Brimble <developer@gregbrimble.com>
2021-10-24 12:17:29 +0100

    Most of a header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,43 @@
+import { ReactNode } from "react";
 import { Link } from "remix";
 import { Logo } from "./Logo";
 import { SmartLink } from "./SmartLink";
 
-const navigation: { name: string; to?: string; href?: string }[] = [
+interface NavigationLink {
+  name: ReactNode;
+  to?: string;
+  href?: string;
+}
+
+const navigation: NavigationLink[] = [
   { name: "Open Source", to: "/open-source" },
   { name: "Data", to: "/data" },
   { name: "Blog", to: "/blog" },
-  { name: "Videos", to: "/live" },
 ];
 
 export const Header = () => {
+  const isLive = false;
+
+  const videoLink: NavigationLink = isLive
+    ? {
+        name: (
+          <span className="flex items-center space-x-3">
+            <span>Live</span>
+            <span
+              className="bg-red-100 dark:bg-red-900 h-4 w-4 rounded-full flex items-center justify-center"
+              aria-hidden="true"
+            >
+              <span className="bg-red-400 dark:bg-red-500 h-2 w-2 rounded-full" />
+            </span>
+          </span>
+        ),
+        to: "/live",
+      }
+    : {
+        name: "Videos",
+        to: "/videos",
+      };
+
   return (
     <header>
       <nav className="px-4 sm:px-6 lg:px-8" aria-label="Top">
@@ -22,9 +50,9 @@ export const Header = () => {
               </svg>
             </Link>
             <div className="hidden ml-10 space-x-8 lg:block">
-              {navigation.map((link) => (
+              {[...navigation, videoLink].map((link) => (
                 <SmartLink
-                  key={link.name}
+                  key={link.to || link.href}
                   to={link.to}
                   href={link.href}
                   className="text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
@@ -36,9 +64,9 @@ export const Header = () => {
           </div>
         </div>
         <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map((link) => (
+          {[...navigation, videoLink].map((link) => (
             <SmartLink
-              key={link.name}
+              key={link.to || link.href}
               to={link.to}
               href={link.href}
               className="text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
---
+++ app/routes/live.tsx
@@ -0,0 +1,7 @@
+export default function Live() {
+  return (
+    <div>
+      <h1>Live</h1>
+    </div>
+  );
+}
