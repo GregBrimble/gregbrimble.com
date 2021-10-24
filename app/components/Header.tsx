e08aa333eb1093be37f0aaba1907b509d142e5d5
Greg Brimble <developer@gregbrimble.com>
2021-10-24 11:02:27 +0100

    No link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,100 +1,53 @@
-import type { AnchorHTMLAttributes } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDev } from "@fortawesome/free-brands-svg-icons/faDev";
-import { faTwitch } from "@fortawesome/free-brands-svg-icons/faTwitch";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
-import MailIcon from "@heroicons/react/outline/MailIcon";
+// import { Link } from "react-router-dom";
+// import { Logo } from "./Logo";
 import { SmartLink } from "./SmartLink";
 
 const navigation = [
-  {
-    name: "LinkedIn",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://www.linkedin.com/in/gregbrimble" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faLinkedin} />,
-  },
-  {
-    name: "GitHub",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://github.com/gregbrimble/" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faGithub} />,
-  },
-  {
-    name: "DEV",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://dev.to/gregbrimble" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faDev} />,
-  },
-  {
-    name: "Twitter",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://twitter.com/gregbrimble" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faTwitter} />,
-  },
-  {
-    name: "Twitch",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://www.twitch.tv/gregbrimble" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faTwitch} />,
-  },
-  {
-    name: "YouTube",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink
-        href="https://www.youtube.com/channel/UCUfprYvJEwcfNCcMnTGxVWA"
-        {...props}
-      />
-    ),
-    icon: <FontAwesomeIcon icon={faYoutube} />,
-  },
-  {
-    name: "Instagram",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="https://www.instagram.com/gregbrimble/" {...props} />
-    ),
-    icon: <FontAwesomeIcon icon={faInstagram} />,
-  },
-  {
-    name: "Email",
-    link: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => (
-      <SmartLink href="mailto:hello@gregbrimble.com" {...props} />
-    ),
-    icon: <MailIcon />,
-  },
+  { name: "Open Source", to: "/open-source" },
+  { name: "Data", to: "/data" },
+  { name: "Blog", href: "https://blog.gregbrimble.com/" },
+  { name: "Newsletter", href: "https://newsletter.gregbrimble.com/" },
 ];
 
 export const Header = () => {
   return (
-    <footer>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 md:flex md:items-center md:justify-between">
-        <div className="flex justify-center space-x-6 md:order-2">
-          {navigation.map((item) => (
-            <item.link
-              key={item.name}
-              className="text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400"
+    <header>
+      <nav className="px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-6 flex items-center justify-between border-b border-gray-500 dark:border-gray-400 lg:border-none">
+          <div className="flex items-center">
+            {/* <Link to="/"> */}
+            <span className="sr-only">gregbrimble.com</span>
+            <svg className="h-10 w-10 text-gray-900 dark:text-white">
+              {/* <Logo /> */}
+            </svg>
+            {/* </Link> */}
+            <div className="hidden ml-10 space-x-8 lg:block">
+              {navigation.map((link) => (
+                <SmartLink
+                  key={link.name}
+                  to={link.to}
+                  href={link.href}
+                  className="text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+                >
+                  {link.name}
+                </SmartLink>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+          {navigation.map((link) => (
+            <SmartLink
+              key={link.name}
+              to={link.to}
+              href={link.href}
+              className="text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
             >
-              <span className="sr-only">{item.name}</span>
-              <svg className="h-6 w-6" aria-hidden="true">
-                {item.icon}
-              </svg>
-            </item.link>
+              {link.name}
+            </SmartLink>
           ))}
         </div>
-        <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-center text-base text-gray-400 dark:text-gray-500">
-            &copy; 2021 gregbrimble.com. All rights reserved.
-          </p>
-        </div>
-      </div>
-    </footer>
+      </nav>
+    </header>
   );
 };
