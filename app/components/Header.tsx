66aeacd5a69c9d042f7cc410d988d0b0ca62942a
Greg Brimble <developer@gregbrimble.com>
2021-10-24 11:05:25 +0100

    Um

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom";
-import { Logo } from "./Logo";
+// import { Logo } from "./Logo";
 import { SmartLink } from "./SmartLink";
 
 const navigation = [
@@ -13,14 +13,14 @@ export const Header = () => {
   return (
     <header>
       <nav className="px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between border-b border-gray-500 dark:border-gray-400 lg:border-none">
+        {/* <div className="w-full py-6 flex items-center justify-between border-b border-gray-500 dark:border-gray-400 lg:border-none">
           <div className="flex items-center">
-            {/* <Link to="/"> */}
-            <span className="sr-only">gregbrimble.com</span>
-            <svg className="h-10 w-10 text-gray-900 dark:text-white">
-              <Logo />
-            </svg>
-            {/* </Link> */}
+            <Link to="/">
+              <span className="sr-only">gregbrimble.com</span>
+              <svg className="h-10 w-10 text-gray-900 dark:text-white">
+                <Logo />
+              </svg>
+            </Link>
             <div className="hidden ml-10 space-x-8 lg:block">
               {navigation.map((link) => (
                 <SmartLink
@@ -34,7 +34,7 @@ export const Header = () => {
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
           {navigation.map((link) => (
             <SmartLink
