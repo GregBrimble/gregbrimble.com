656360b6b7b392c133732dd730a6d2dc2745b29e
Greg Brimble <developer@gregbrimble.com>
2021-10-25 23:08:11 +0100

    Blog complete

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -7,6 +7,7 @@ import { formatDate } from "~/utils/formatDate";
 
 import { indexLoader as initialization } from "./blog/initialization";
 import { indexLoader as optimizingImages } from "./blog/optimizing-images";
+import { ExternalLink } from "~/components/ExternalLink";
 
 const IS_BLOG_POST_REGEXP = /^\/blog\/.+/i;
 
@@ -36,14 +37,18 @@ export type IndexLoader = (context: AppLoadContext) => Promise<Writing>;
 
 const posts = [initialization, optimizingImages];
 
-export const loader: LoaderFunction = async ({ context }) => {
-  try {
-    const loadedPosts = await Promise.all(posts.map((post) => post(context)));
-    return loadedPosts.sort(
-      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-    );
-  } catch {}
-  return [];
+export const loader: LoaderFunction = async ({ context, request }) => {
+  const loadedWritings = (
+    await Promise.allSettled(posts.map((post) => post(context)))
+  )
+    .map((promise) =>
+      promise.status === "fulfilled" ? promise.value : undefined
+    )
+    .filter((writing) => writing !== undefined) as Writing[];
+
+  return loadedWritings.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
 };
 
 const BlogIndex = () => {
@@ -54,40 +59,59 @@ const BlogIndex = () => {
       <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 dark:divide-gray-700 lg:max-w-7xl">
         <div>
           <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-4xl">
-            From the blog
+            Writings
           </h2>
           <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
             <p className="text-xl text-gray-500 dark:text-gray-400">
-              A collection of posts primarily about technology.
+              A collection of articles primarily about technology.
             </p>
             <form
-              className="mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end"
+              className="mt-6 lg:mt-0"
               action="https://newsletter.gregbrimble.com/add_subscriber"
               method="post"
               name="revue-form"
               target="_blank"
             >
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="member[email]"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="appearance-none w-full px-4 py-2 border border-gray-300 dark:border-gray-600 text-base rounded-md text-gray-900 dark:text-white bg-white dark:bg-gray-900 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-blue-500 dark:focus:ring-blue-400 focus:border-blue-500 dark:focus:border-blue-400 lg:max-w-xs"
-                  placeholder="Enter your email"
-                />
+              <div className="flex flex-col sm:flex-row lg:justify-end">
+                <div className="flex-1 lg:flex-initial">
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="member[email]"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none w-full px-4 py-2 border border-gray-300 dark:border-gray-600 text-base rounded-md text-gray-900 dark:text-white bg-white dark:bg-gray-900 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-blue-500 dark:focus:ring-blue-400 focus:border-blue-500 dark:focus:border-blue-400 lg:max-w-xs"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto sm:inline-flex">
+                  <input
+                    type="submit"
+                    value="Notify me"
+                    name="member[subscribe]"
+                    className="w-full bg-blue-600 dark:bg-blue-300 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white dark:text-gray-900 hover:bg-blue-700 dark:hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-blue-400 sm:w-auto sm:inline-flex cursor-pointer"
+                  />
+                </div>
               </div>
-              <div className="mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto sm:inline-flex">
-                <input
-                  type="submit"
-                  value="Notify me"
-                  name="member[subscribe]"
-                  className="w-full bg-blue-600 dark:bg-blue-300 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white dark:text-gray-900 hover:bg-blue-700 dark:hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-blue-400 sm:w-auto sm:inline-flex cursor-pointer"
-                />
+              <div className="mt-4 sm:mt-2 text-sm text-gray-500 dark:text-gray-400 lg:text-right">
+                By subscribing, you agree with Revue’s{" "}
+                <ExternalLink
+                  href="https://www.getrevue.co/terms"
+                  className="underline"
+                >
+                  Terms of Service
+                </ExternalLink>{" "}
+                and{" "}
+                <ExternalLink
+                  href="https://www.getrevue.co/privacy"
+                  className="underline"
+                >
+                  Privacy Policy
+                </ExternalLink>
+                .
               </div>
             </form>
           </div>
@@ -123,7 +147,7 @@ const BlogIndex = () => {
               </div>
               <div className="mt-3">
                 <p className="text-base font-semibold text-blue-600 dark:text-blue-300 hover:text-blue-500 dark:hover:text-blue-400">
-                  Read the full post
+                  Read the full article
                 </p>
               </div>
             </Link>
