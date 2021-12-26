cf028677447ec1832bd15ca5dd730e36d38202ce
Greg Brimble <developer@gregbrimble.com>
2021-12-26 17:54:18 +0000

    Linting etc.

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,7 +1,7 @@
 // https://github.com/remix-run/remix/pull/1237
 // import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import { createPagesFunctionHandler } from "./worker";
-import { attachClients } from "../app/data";
+import { attachClients } from "~/data";
 import { Context } from "~/types";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
